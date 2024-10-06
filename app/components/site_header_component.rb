d3bc50fe9556c705a5cadcea5f2ffdfcd5cdefd5
Julian Rubisch <julian@julianrubisch.at>
2024-10-06 15:17:28 +0200

    chore: Rewrite SiteHeader with Phlex components

--- app/components/site_header_component.rb
+++ app/components/site_header_component.rb
@@ -3,13 +3,6 @@
 class Components::SiteHeaderComponent < Components::Base
   include Phlex::Rails::Helpers::LinkTo
 
-  register_element :wa_dropdown
-  register_element :wa_button
-  register_element :wa_menu
-  register_element :wa_menu_item
-  register_element :wa_icon
-  register_element :wa_divider
-
   def view_template
     nav(class: "level p-4") {
       ul(class: "level-left") {
@@ -21,18 +14,19 @@ class Components::SiteHeaderComponent < Components::Base
         }
       }
       div(class: "level-right") {
-        wa_dropdown(placement: "bottom-start") {
-          wa_button(slot: :trigger, appearance: :tinted, size: :small, pill: true, caret: true) {
-            wa_icon(slot: "prefix", name: "sun", variant: "solid")
-            plain "Light"
-          }
-          wa_menu(data: { action: "wa-select->dark-mode#toggle" }) {
-            wa_menu_item(value: :light, type: :checkbox) { "Light" }
-            wa_menu_item(value: :dark, type: :checkbox) { "Dark" }
-            wa_divider
-            wa_menu_item(value: :auto, type: :checkbox) { "System" }
-          }
-        }
+        render WebAwesome::WaDropdown.new(placement: "bottom-start") do |dropdown|
+                      render WebAwesome::WaButton.new(slot: :trigger, appearance: :tinted, size: :small, pill: true, caret: true) do |button|
+                        render WebAwesome::WaIcon.new(slot: "prefix", name: "sun", variant: "solid")
+                        plain "Light"
+                      end
+
+                      render WebAwesome::WaMenu.new(data: { action: "wa-select->dark-mode#toggle" }) do
+                        render WebAwesome::WaMenuItem.new(value: :light, type: :checkbox) { "Light" }
+                        render WebAwesome::WaMenuItem.new(value: :dark, type: :checkbox) { "Dark" }
+                        render WebAwesome::WaDivider.new
+                        render WebAwesome::WaMenuItem.new(value: :auto, type: :checkbox) { "System" }
+                      end
+        end
       }
     }
   end
