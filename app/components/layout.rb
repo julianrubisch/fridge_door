567e91a06deac2efd444521fd5be039c6f6e1394
Julian Rubisch <julian@julianrubisch.at>
2024-10-16 18:10:28 +0200

    chore: Allow to customize brand colors

--- app/components/layout.rb
+++ app/components/layout.rb
@@ -16,8 +16,8 @@ class Components::Layout < Components::Base
         meta name: "viewport", content: "width=device-width,initial-scale=1"
         csp_meta_tag
         csrf_meta_tags
-        stylesheet_link_tag "application", data_turbo_track: "reload"
         stylesheet_link_tag "https://early.webawesome.com/webawesome@3.0.0-alpha.4/dist/themes/default.css"
+        stylesheet_link_tag "application", data_turbo_track: "reload"
         javascript_include_tag "https://early.webawesome.com/webawesome@3.0.0-alpha.4/dist/webawesome.loader.js", type: "module", async: true, defer: true
 
         script {
