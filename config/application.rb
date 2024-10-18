61485994bdcb92e3f01e3eb84fbbee16d451e78d
Julian Rubisch <julian@julianrubisch.at>
2024-10-18 09:30:32 +0200

    feat: Finish scaffold generator

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Weisslich
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
+    config.app_generators.scaffold_controller :phlex_controller
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
