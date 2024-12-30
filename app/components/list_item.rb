5f162bb3464ea49a75af004726b93832c14ffc75
Julian Rubisch <julian@julianrubisch.at>
2024-12-30 17:30:35 +0100

    fix: List item template

--- app/components/list_item.rb
+++ app/components/list_item.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
 class Components::ListItem < Components::Base
-  def view_template
-    h1 { "ListItem" }
-    p { "Find me in app/components/list_item.rb" }
+  def initialize(id:)
+    @id = id
+  end
+
+  def view_template(&)
+    div(id: @id, class: "wa-split") {
+      render Components::WebAwesome::WaCard.new(class: "", with_header: false, with_footer: false, with_image: false) {
+        div(class: "wa-flank:end wa-align-items-center", &)
+      }
+    }
+  end
+
+  def actions(gap: "m", &)
+    div(class: "wa-stack wa-align-items-end wa-gap-#{gap}", &)
   end
 end
